3a956e22fe4004f1abaa24a24a13ab59e1a35571
Tharkana Kodagoda <>
2018-09-17 18:18:12 +0530

    Added per user message support

--- server-side/index.js
+++ server-side/index.js
@@ -1,25 +1,51 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+let users = {};
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
 
+io.on("connection", function(socket) {
+  let query = socket.request._query;
+  let user = {
+    username: query.username,
+    // uid: query.uid,
+    socket_id: socket.id
+  };
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+  updateUserList(user);
+  
+  console.log("a user connected");
 
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+    if(users[socket.id]){
+      delete users[socket.id];
+      updateUserList(null);
+    }
+    
+  });
 
-      socket.on('SEND_MESSAGE', function(msg){
-        console.log('message: ' + msg ? msg.message : msg);
-        io.emit('RECEIVE_MESSAGE', msg);
-      });
+  socket.on("SEND_MESSAGE", (msg) => {
+    console.log("message: " + JSON.stringify(msg)  );
+    io.to(msg.socket_id).emit("RECEIVE_MESSAGE", msg.message);
+  });
 });
+
+updateUserList = user => {
+
+  if(user){
+    users[user.socket_id] =  user;
+  }
+   
+  console.log(JSON.stringify(users));
+
+  io.emit("USER_LIST",  Object.values(users) );
+};
